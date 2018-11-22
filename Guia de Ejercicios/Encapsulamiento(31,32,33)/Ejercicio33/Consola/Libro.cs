9f6ec4d9b5ce51b17d3d99ee0f96e6a2e171fa39
Matías Cáceres <matias.caceres@linio.com>
2018-11-22 16:30:05 -0300

    agregados

---
+++ Guia de Ejercicios/Encapsulamiento(31,32,33)/Ejercicio33/Consola/Libro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola
+{
+    public class Libro
+    {
+        private List<string> paginas = new List<string>();
+
+        public List<string> Paginas
+        {
+            get
+            {
+                return this.paginas;
+            }
+        }
+
+        public string this[int i]
+        {
+            get
+            {
+                return paginas[i];
+            }
+            set
+            {
+                if (i >= (paginas.Count - 1))
+                    paginas.Add(value);
+                else
+                    paginas[i] = value;
+            }
+        }
+    }
+}
---
+++ Guia de Ejercicios/Encapsulamiento(31,32,33)/Ejercicio33/Consola/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro libro = new Libro();
+            libro[0] = "hola";
+            libro[3] = "que haces?";
+            libro[2] = "bro";
+            libro[10] = "hhh";
+
+            for(int i = 0; i <= libro.Paginas.Count-1; i++)
+            { 
+                Console.WriteLine(libro[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
